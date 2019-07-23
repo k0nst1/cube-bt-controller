27642208d11cf5edcfc3a8e28bc972112a7207b3
Konstantin Vogel <k.vogel@epubli.com>
2019-07-23 21:43:26 +0200

    stuff...

--- src/models/procedure.ts
+++ src/models/procedure.ts
@@ -8,20 +8,19 @@ export class Procedure {
     constructor() {
         this.entry = [];
         this.entry.push(new Mode("asd",255,255,0,0,{hue: 23,saturation: 25,value: 255},60,15))
-        this.entry.push(new Mode("asd",55,55,0,0,{hue: 50,saturation: 255,value: 190},60,17))
+        this.entry.push(new Mode("asd",55,55,0,0,{hue: 50,saturation: 255,value: 255},60,17))
         this.entry.push(new Mode("asd",90,150,0,0,{hue: 39,saturation: 47,value: 255},60,21))
-        this.entry.push(new Mode("asd",255,255,0,0,{hue: 178,saturation: 180,value: 100},60,10))
+        this.entry.push(new Mode("asd",255,255,0,0,{hue: 178,saturation: 180,value: 255},60,10))
         this.entry.push(new Mode("asd",255,230,0,0,{hue: 0,saturation: 150,value: 255},60,13))
         this.entry.push(new Mode("asd",200,190,0,0,{hue: 255,saturation: 255,value: 255},60,16))
-        this.entry.push(new Mode("asd",100,255,0,0,{hue: 190,saturation: 140,value: 190},120,2))
+        this.entry.push(new Mode("asd",100,255,0,0,{hue: 190,saturation: 140,value: 255},120,2))
         this.entry.push(new Mode("asd",255,255,0,0,{hue: 255,saturation: 255,value: 255},60,17))
-        this.entry.push(new Mode("asd",180,18,0,0,{hue: 0,saturation: 90,value: 130},45,10))
+        this.entry.push(new Mode("asd",180,18,0,0,{hue: 0,saturation: 90,value: 255},45,10))
         this.entry.push(new Mode("asd",100,30,0,0,{hue: 0,saturation: 125,value: 200},30,6));
-        this.entry.push(new Mode("asd",60,110,0,0,{hue: 160,saturation: 255,value: 170},15,9));
-        this.entry.push(new Mode("asd",180,18,0,0,{hue: 90,saturation: 90,value: 130},60,10))
+        this.entry.push(new Mode("asd",180,18,0,0,{hue: 90,saturation: 90,value: 255},60,10))
         this.entry.push(new Mode("asd",43,255,0,0,{hue: 39,saturation: 47,value: 255},60,14))
         this.entry.push(new Mode("asd",100,255,0,0,{hue: 255,saturation: 140,value: 190},60,2))
-        this.entry.push(new Mode("asd",180,255,0,0,{hue: 0,saturation: 90,value: 130},60,10))
+        this.entry.push(new Mode("asd",180,255,0,0,{hue: 0,saturation: 90,value: 255},60,10))
         this.entry.push(new Mode("asd",90,0,0,0,{hue: 100,saturation: 185,value: 255},40,20))
 
 
